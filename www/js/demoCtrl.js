9f867d575a486f7b9c2f9347d26fb5a37323dc8e
Chris Perko <cperko@teachers-teachers.com>
2016-01-03 22:37:27 -0600

    code cleanup

--- www/js/demoCtrl.js
+++ www/js/demoCtrl.js
@@ -1,6 +1,8 @@
 angular.module('localNotifications')
 .controller('DemoCtrl', function($cordovaLocalNotification, $ionicPlatform, DemoService) {
+
   this.showNotification = function() {
     DemoService.showNotification('Notify!', 'This is a test!');
-  }
+  };
+
 });
